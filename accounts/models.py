4ea6c34f3b4f00460dbd6f04d9cff487d4a40756
mshallar <83505425+mshallar@users.noreply.github.com>
2023-01-27 23:26:10 +0300

    Initial preparation and processing of views and models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# class User(AbstractUser):
+#     class Role(models.TextChoices):
+#         ADMIN = "ADMIN", 'Admin'
+#         STUDENT = "STUDENT", 'Student'
+#         TEACHER = "TEACHER", 'Teacher'
+
+#     base_role = Role.ADMIN
+#     role = models.CharField(max_length=50, choices=Role.choices)
--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Courses, Topics, Order, Comment
+
+admin.site.register(Courses)
+admin.site.register(Topics)
+admin.site.register(Order)
+admin.site.register(Comment)
